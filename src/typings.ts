029453f9b83fa676d03179ea0e43aa16d569233c
David Schkalee <david.schkalee@magicline.de>
2018-07-12 13:25:36 +0200

    add typings

---
+++ src/typings.ts
@@ -0,0 +1,18 @@
+export interface ReactTimebombProps {
+    value?: Date;
+    format?: string;
+    placeholder?: string;
+    minDate?: Date;
+    maxDate?: Date;
+    onChange(date?: Date): void;
+    onError?(error: ReactTimebombError, value: string): void;
+}
+
+export interface ReactTimebombState {
+    valueText?: string;
+    date: Date;
+    mode: 'year' | 'months' | 'month';
+    showTime?: boolean;
+}
+
+export type ReactTimebombError = 'outOfRange' | 'invalidDate';
