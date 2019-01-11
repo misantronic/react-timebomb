8fd72fba56db37a5f50b02bbd42ad1f1a877c397
David Schkalee <david.schkalee@magicline.de>
2019-01-11 14:43:10 +0100

    export ButtonProps

--- src/button.tsx
+++ src/button.tsx
@@ -2,7 +2,7 @@
 import * as React from 'react';
 import styled, { css } from 'styled-components';
 
-interface ButtonProps {
+export interface ButtonProps {
     selected?: boolean;
     mobile?: boolean;
 }
